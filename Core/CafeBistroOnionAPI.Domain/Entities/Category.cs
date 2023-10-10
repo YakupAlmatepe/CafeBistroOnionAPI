d4ed4bc79f6c7f540df1fe78b89cfd0bfe3bae6a
Yakup Almatepe <almatepe.yakup@gmail.com>
2023-10-10 18:16:03 +0300

    entities

---
+++ Core/CafeBistroOnionAPI.Domain/Entities/Category.cs
@@ -0,0 +1,30 @@
+﻿using CafeBistroOnionAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBistroOnionAPI.Domain.Entities
+{
+	public class Category : EntityBase,IEntityBase
+	{
+		
+			public Category()
+			{
+
+			}
+			public Category(int parentId, string name, int priorty)
+			{
+				ParentId = parentId;
+				Name = name;
+				Priorty = priorty;
+			}
+			public int ParentId { get; set; }
+			public string Name { get; set; }
+			public int Priorty { get; set; }
+			public ICollection<Detail> Details { get; set; }
+			public ICollection<ProductCategory> ProductCategories { get; set; }
+		}
+	}
+}
---
+++ Core/CafeBistroOnionAPI.Domain/Entities/OurFoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBistroOnionAPI.Domain.Entities
+{
+	public class OurFoot
+	{
+		public int OurDishId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Breakfast { get; set; }
+		public string Lunch { get; set; }
+		public string Dinner { get; set; }
+		public decimal BreakfastPrice { get; set; }
+		public decimal LunchPrice { get; set; }
+		public decimal DinnerPrice { get; set; }
+		public string ImageUrl1 { get; set; }
+		public string ImageUrl2 { get; set; }
+	}
+}
